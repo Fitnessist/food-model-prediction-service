bbef0833901571760c7bb0b411c0a77e534ec93e
triadi <triadilaksamana@gmail.com>
2023-06-06 00:26:08 +0700

    get model from storage

--- routes.py
+++ routes.py
@@ -1,10 +1,28 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
+import os
+from PIL import Image
+from google.cloud import storage
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".credentials\credential.json"
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('models/model.h5')
+client = storage.Client()
+
+# Mendownload model dari GCS
+bucket_name = "bucket-training-model"
+blob_name = "foodModel1.h5"
+model_path = "/tmp/foodModel1.h5"  # Path sementara untuk menyimpan model
+bucket = client.get_bucket(bucket_name)
+blob = bucket.blob(blob_name)
+blob.download_to_filename(model_path)
+
+model = tf.keras.models.load_model(model_path)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# hapus model sementara
+os.remove(model_path)
 
 # Define class labels
 class_labels = ['class1', 'class2', 'class3', 'class4', 'class5']
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from routes import app
 
 if __name__ == '__main__':
