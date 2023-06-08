34d3425a17c98bc6c4bacae91a957e5f6a11977d
triadi <triadilaksamana@gmail.com>
2023-06-08 10:04:00 +0700

    fix calories

--- app/predictions/food/model.py
+++ app/predictions/food/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from app.infrastructure.database import Base, get_db
 from functools import lru_cache
 
@@ -7,11 +7,13 @@ class Food(Base):
 
     id = Column(String(40), primary_key=True)
     food_name = Column(String(100), nullable=False)
+    calories_per_100gr = Column(Integer())
     image_url = Column(String(255), nullable=False, unique=True)
 
-    def __init__(self, id, food_name, image_url):
+    def __init__(self, id, food_name, calories_per_100gr, image_url):
         self.id = id
         self.food_name = food_name
+        self.calories_per_100gr = calories_per_100gr
         self.image_url = image_url
     
 
@@ -26,6 +28,7 @@ class Food(Base):
                 food_entry = {
                     'id': food.id,
                     'food_name': food.food_name,
+                    'calories_per_100gr': food.calories_per_100gr,
                     'image_url': food.image_url
                 }
                 food_list.append(food_entry)
